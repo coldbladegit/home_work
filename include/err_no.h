90903c1ad14f2ee82cc9f132d317958babd2322d
coldblade <cold_blade@126.com>
2017-09-01 18:28:39 +0800

    home work

---
+++ include/err_no.h
@@ -0,0 +1,18 @@
+#ifndef _ERR_NO_H
+#define _ERR_NO_H
+
+#define ERR_SUCCESS             0
+#define ERR_FAILED              -1
+#define ERR_INVALID_PARAM       -2
+#define ERR_MALLOC_MEM_FAILD    100
+#define ERR_OPEN_FILE_FAILD     101
+#define ERR_READ_FILE_FAILED    102
+#define ERR_EOF_FILE            103
+#define ERR_INVALID_OP          104
+#define ERR_WORD_TOO_LONG       105
+#define ERR_STACK_FULL          106
+#define ERR_STACK_EMPTY         107
+#define ERR_STR_TOO_LONG        108
+#define ERR_NOT_FIND            109
+
+#endif//_ERR_NO_H
